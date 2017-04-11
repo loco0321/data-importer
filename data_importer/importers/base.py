9f3b21d50af961a03176713b62e33918412905f6
Leonardo Ortega Hernandez <leon9894@gmail.com>
2017-04-11 15:08:42 -0500

    chnage in base.oy

--- data_importer/importers/base.py
+++ data_importer/importers/base.py
@@ -168,12 +168,12 @@ class BaseImporter(object):
         User default django field validators to clean content
         and run custom validates
         """
+        pre_clean_function = getattr(self, 'pre_clean_%s' % field_name, False)
+        if pre_clean_function:
+            value = pre_clean_function(value, values=values)
         if self.Meta.model:
             # default django validate field
             try:
-                pre_clean_function = getattr(self, 'pre_clean_%s' % field_name, False)
-                if pre_clean_function:
-                    value = pre_clean_function(value, values=values)
                 field = self.Meta.model._meta.get_field(field_name)
                 field.clean(value, field)
             except FieldDoesNotExist:
